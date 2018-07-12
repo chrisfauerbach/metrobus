f2f416f083d93922484dcd66d03e0ac824cdd381
Chris Fauerbach <chrisfauerbach@gmail.com>
2018-07-12 11:44:03 -0400

    Ticking up version and fixing environment config for 'test'

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="metrobus",
-    version="0.0.4",
+    version="0.0.5",
     author="Chris Fauerbach",
     author_email="chris@fauie.com",
     description="A package to allow flexible and quick programs on a message bus/kafka.",
