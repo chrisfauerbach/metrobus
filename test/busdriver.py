87064a503c291d75b8cceb6c53f1b9ea88df66e0
Chris Fauerbach <chris.fauerbach@capitalone.com>
2018-08-15 16:08:10 -0400

    Cleaning up bus driver

--- test/busdriver.py
+++ test/busdriver.py
@@ -4,33 +4,24 @@ from metrobus import metrobus
 import sys
 import random
 
-
-# To consume latest messages and auto-commit offsets
-
 def callback(message):
-    #print("Received in CB: ", message)
-    real_message = message#.value
+    real_message = message
 
     # This is the first stop.. no headers, wrappers, etc.
     # Time to add them.   This is going to get a little weird.
     outbound_message = {}
     header = {}
 
-
     # For now, everything will go through AddEmail, WhiteList, Log
     routes = []
     routes.insert(0, 'AddEmail')
     routes.insert(0, 'WhiteList')
     routes.insert(0, 'Log')
-    #print("Routes: ", routes)
     header[metrobus.ROUTE_FIELD]          = routes
     header[metrobus.ORIGINAL_ROUTE_FIELD] = routes.copy()
     header[metrobus.HISTORICAL_ROUTE_FIELD] = []
-    ##print("Header: ", header)
-    #print("Body: ", real_message)
     outbound_message[metrobus.HEADER_FIELD] = header
     outbound_message[metrobus.BODY_FIELD] = real_message
-    #print("Leaving CB with: ", outbound_message)
     return outbound_message 
 
 if __name__ == "__main__":
