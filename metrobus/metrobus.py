a7aefb87428ea7f8f1d58327db3a659f45ffa7e1
Chris Fauerbach <chris.fauerbach@capitalone.com>
2018-08-22 16:02:37 -0400

    adding changes to enable more visibility at the microservice

--- metrobus/metrobus.py
+++ metrobus/metrobus.py
@@ -27,6 +27,8 @@ REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
 
 CACHE = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
+logger = logging.getLogger(__name__)
+
 class BadStateRecordError(Exception):
     """Raised when an operation attempts a state transition that's not
     allowed.
@@ -59,38 +61,34 @@ class MetroStop(object):
         self.consumer = None
         self.producer = None
         self.topic_name = in_topic # "Source"
-        logging.info("Input topic: %s", self.topic_name)
+        logger.info("Input topic: %s", self.topic_name)
         self.error_topic = 'Error'
         self.callback = callback
         self.consumer = None
-        logging.info("Looking for consumer in bg thread.")
-        logging.info(f"Config:\nKafka: {KAFKA_HOST}:{KAFKA_PORT}\nRedis: {REDIS_HOST}:{REDIS_PORT}")
+        logger.info("Looking for consumer in bg thread.")
+        logger.info(f"Config:\nKafka: {KAFKA_HOST}:{KAFKA_PORT}\nRedis: {REDIS_HOST}:{REDIS_PORT}")
         #self.consumer = self.get_consumer()
         #logging.info("Started background thread with consumer: %s", self.consumer)
         #self.producer = self.get_producer()
         #logging.info("Starting background thread with producer: %s", self.producer)
-        self.setLastMessage(None)
 
-    def setLastMessage(self, message):
-        self.last_message = message
-
-    def push_stop(self, stop):
+    def push_stop(self, message, stop):
         routes = []
         message_header = None
 
-        if self.last_message:
-            message_header = self.last_message.get(HEADER_FIELD)
+        if message:
+            message_header = message.get(HEADER_FIELD)
             if message_header:
                 routes = message_header.get(ROUTE_FIELD)
         if routes:
             routes.insert(0, stop)
 
-    def pop_stop(self):
+    def pop_stop(self, message):
         routes = []
         message_header = None
 
-        if self.last_message:
-            message_header = self.last_message.get(HEADER_FIELD)
+        if message:
+            message_header = message.get(HEADER_FIELD)
             if message_header:
                 routes = message_header.get(ROUTE_FIELD)
         if routes:
@@ -103,89 +101,88 @@ class MetroStop(object):
         for _ in range(4):
             try:
                 self.consumer = KafkaConsumer(self.topic_name,
-                                              group_id='my-group',
-                                              consumer_timeout_ms=30000,
+                                              group_id=os.environ.get('CONSUMER_GROUP', "metrobus"),
+                                              consumer_timeout_ms=10000,
+                                              auto_offset_reset='earliest',
                                               bootstrap_servers=[f"{KAFKA_HOST}:{KAFKA_PORT}"],
                                               value_deserializer=lambda v: json.loads(v))
+                return self.consumer
             except kafka.errors.NoBrokersAvailable:
                 pass
             except: 
-                logging.exception("Error in consumer") 
+                logger.exception("Error in consumer") 
             time.sleep(3)
         if not self.consumer:
             raise Exception("Missing consumer.")
 
-        return self.consumer
 
     def get_producer(self):
         if self.producer:
             return self.producer
 
-        for _ in range(4):
+        for _ in range(10):
             try:
                 self.producer = KafkaProducer(bootstrap_servers=['kafka:9092'],
                                               value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+                return self.producer
             except kafka.errors.NoBrokersAvailable:
                 pass
             except:
-                logging.exception("Error in producer") 
+                logger.exception("Error in producer") 
                 time.sleep(3)
         if not self.producer:
             raise Exception("Missing producer.")
-        return self.producer
 
     def start(self):
         try:
             self._start()
         except:
-            logging.exception("There was an uncaught exception during processing.")
+            logger.exception("There was an uncaught exception during processing.")
 
     def _start(self):
-        logging.info("Starting thread. listening to: ", self.topic_name)
+        logger.info("Starting thread. listening to: %s", self.topic_name)
         consumer = self.get_consumer() 
         producer = self.get_producer()
         if consumer:
             while True:
                 for kafka_message in consumer:
-                    logging.debug("%s:%s:%s: key=%s value=%s",kafka_message.topic, kafka_message.partition, kafka_message.offset, kafka_message.key, kafka_message.value)
+                    logger.debug("%s:%s:%s: key=%s value=%s",kafka_message.topic, kafka_message.partition, kafka_message.offset, kafka_message.key, kafka_message.value)
                     self.add_count(kafka_message.topic)
                     message = kafka_message.value 
                     message_payload = message.get(PAYLOAD_FIELD, message)
                     
                     if self.callback:   
-                        # retval means the callback returned something
-                        returned_body = self.callback(message_payload)  
+                        # returned_body means the callback returned something
+                        returned_body = self.callback(message_payload, full_message=message)  
                         if returned_body: # and (local_out_topic):
                             #This may happen if its a new message (since above has no head/body)
-                            #but the retval may have is, inserted by busdriver.   
+                            #but the returned_body may have is, inserted by busdriver.   
                             # @TODO  change how this works.
                             if HEADER_FIELD in returned_body:
-                                message = retval
-                                message_header = message.get(HEADER_FIELD)
+                                message = returned_body
                             else:
-                                #logging.error("NO MESSAGE HEADER, I think this is where I add one.?????")
-                                message[PAYLOAD_FIELD] = retval
+                                #logger.error("NO MESSAGE HEADER, I think this is where I add one.?????")
+                                message[PAYLOAD_FIELD] = returned_body
 
-                            self.setLastMessage( message  )
-                            next_stop = self.pop_stop()
+                            message_header = message.get(HEADER_FIELD)
+                            next_stop = self.pop_stop(message)
                             if next_stop:
                                 message_header.get(HISTORICAL_ROUTE_FIELD).append(next_stop)
-                                logging.info(f"Downstream, now sending to %s %s", next_stop, message)
+                                logger.info(f"Downstream, now sending to %s %s", next_stop, message)
                                 producer.send(next_stop, message)
                             else:
-                                logging.error("NO NEXT STOP 1: %s", message.value)
-                                logging.error("NO NEXT STOP 2: %s", message)
-                                logging.error("NO NEXT STOP 2.5:%s", message_payload)
-                                logging.error("NO NEXT STOP 3: %s", message)
+                                logger.error("NO NEXT STOP 1: %s", message.value)
+                                logger.error("NO NEXT STOP 2: %s", message)
+                                logger.error("NO NEXT STOP 2.5:%s", message_payload)
+                                logger.error("NO NEXT STOP 3: %s", message)
                                 sys.exit(1)
-                        else: #No retval!
-                            logging.info("Nothing returned, therefore, assume filtered.")
-                    self.setLastMessage( None  )
-                logging.info("No message detected yet.")
+                        else: #No returned_body!
+                            logger.info("Nothing returned, therefore, assume filtered.")
+                logger.info("No message detected yet.")
         else:
-            logging.error("SOMETHING IS BROKEN. NO CONSUMER")
+            logger.error("SOMETHING IS BROKEN. NO CONSUMER")
             sys.exit(1)
-        logging.error("Uh Oh. I am exiting for some reason.")
+        logger.error("Uh Oh. I am exiting for some reason.")
 
     def add_count(self, topic):
         return CACHE.hincrby(COUNTER_KEY, topic, 1)
@@ -194,15 +191,15 @@ class MetroStop(object):
 if __name__ == "__main__":
     logging.info("Trying to start app.")
     metro = MetroStop()
-    metro.setLastMessage({
+    message = {
         HEADER_FIELD: {
             ROUTE_FIELD:[1,2,3,4]
         },
         PAYLOAD_FIELD: {"one":"two", "three":3 }
-    })
-    print(metro.pop_stop())
-    print(metro.pop_stop())
-    print(metro.pop_stop())
+    }
+    print(metro.pop_stop(message))
+    print(metro.pop_stop(message))
+    print(metro.pop_stop(message))
     
 
 
